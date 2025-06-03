3e11d8dc1bc9ad638b4d868a2b6c5a93cff076a5
cio <chio.orlands@gmail.com>
2025-06-03 17:15:42 +0700

    modified:   app/Models/Herbisidagroup.php

--- app/Models/Herbisidagroup.php
+++ app/Models/Herbisidagroup.php
@@ -7,19 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Herbisidagroup extends Model
 {
     protected $table = 'herbisidagroup';
-    public $timestamps = false;
 
-    public $incrementing = false;        // kalau key-nya string/non-numeric
-    protected $keyType = 'string';
+    protected $primaryKey = 'herbisidagroupid';
+
+    public $incrementing = false;
+
+    protected $keyType = 'int';
+
+    public $timestamps = false;
 
     protected $fillable = [
         'herbisidagroupid',
         'herbisidagroupname',
-        'activitycode',
         'description',
     ];
-
-    
-
 
 }
