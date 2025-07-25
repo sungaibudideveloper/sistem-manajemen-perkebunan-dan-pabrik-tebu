e926081c719e2dc01f535b909951628514056dcd
Renaldo Livando <renaldolivando6@gmail.com>
2025-07-25 16:47:37 +0700

    Update LiveChat duplicate

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="user-id" content="{{ auth()->user()->userid }}">
     <meta name="current-username" content="{{ Auth::user()->usernm }}">
     
     <!-- Preload font awesome untuk prevent icon flash -->
