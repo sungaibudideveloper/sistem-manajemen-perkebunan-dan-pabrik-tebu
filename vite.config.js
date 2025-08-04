47de9c83d6fcb6b64da3aa02f027fb2079627c50
Renaldo Livando <renaldolivando6@gmail.com>
2025-08-04 10:38:11 +0700

    Update CSS for login (remove CDN)

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css', 
+                'resources/css/login.css',
                 'resources/js/app.js',
                 'resources/js/app.tsx'
             ],
