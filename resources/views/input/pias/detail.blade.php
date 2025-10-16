19cc77ac549d4c17fc33ee4778e88e9b016165db
cio <chio.orlands@gmail.com>
2025-10-16 14:41:01 +0700

    modified:   resources/views/input/pias/detail.blade.php

--- resources/views/input/pias/detail.blade.php
+++ resources/views/input/pias/detail.blade.php
@@ -198,7 +198,7 @@
           <a href="{{ url('input/pias') }}" 
           class="bg-white inline-block bg-gray-200 text-gray-800 hover:bg-gray-300 font-semibold py-2 px-4 rounded shadow transition no-print">
            ← Kembali
-          </a>
+          </a> &nbsp;
           <button
               type="submit"
               class="bg-green-600 hover:bg-green-700 text-white font-semibold py-2 px-6 rounded shadow transition"
