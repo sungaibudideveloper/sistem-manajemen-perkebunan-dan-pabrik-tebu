92020754bf83ad5a27073fe1378f413ebbcd8d9f
Renaldo Livando <renaldolivando6@gmail.com>
2025-10-29 14:39:16 +0700

    Update Create RKH, rows from 8 to 20

--- resources/views/input/rencanakerjaharian/create.blade.php
+++ resources/views/input/rencanakerjaharian/create.blade.php
@@ -305,7 +305,7 @@
             </thead>
 
             <tbody class="divide-y divide-gray-100">
-              @for ($i = 0; $i < 8; $i++)
+              @for ($i = 0; $i < 20; $i++)
                 <tr x-data="activityPicker({{ $i }})" 
                     class="rkh-row hover:bg-blue-50 transition-colors" 
                     x-init="
