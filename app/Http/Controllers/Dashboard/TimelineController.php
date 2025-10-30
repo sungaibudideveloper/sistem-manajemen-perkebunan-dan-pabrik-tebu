fd1fd6f792286a4e11bbacd550bc3cbc4ebb5af7
chio <chio.orlands@gmail.com>
2025-10-30 16:27:37 +0700

    modified:   app/Http/Controllers/Dashboard/TimelineController.php 	modified:   resources/views/dashboard/timeline-plot/index.blade.php

--- app/Http/Controllers/Dashboard/TimelineController.php
+++ app/Http/Controllers/Dashboard/TimelineController.php
@@ -27,15 +27,63 @@ class TimelineController extends Controller
       ]);
     }
 
-    public function plot()
-    {
-      $title = "Dashboard Timeline";
-      $nav = "Timeline";
-
-      return view('dashboard.timeline-plot.index')->with([
+   
+public function plot(Request $request)
+{
+    $title = "Dashboard Timeline";
+    $nav = "Timeline";
+    
+    $rkhno = $request->rkhno ?? 'RKH21050234';
+    
+    // Get data from usematerialhdr
+    $usematerialhdr = new usematerialhdr;
+    $details = $usematerialhdr->selectuse(session('companycode'), $rkhno, 1)->get();
+    $detailsPlots = Arr::pluck($details, 'plot');
+    
+    // Get GPS and plot data
+    $plotData = DB::table('testgpslst as a')
+        ->leftJoin('plot as b', 'a.plot', '=', 'b.plot')
+        ->leftJoin('masterlist as c', 'b.plot', '=', 'c.plot')
+        ->leftJoin('testgpshdr as d', 'a.plot', '=', 'd.plot')
+        ->where('a.companycode', session('companycode'))
+        ->whereIn('a.plot', $detailsPlots)
+        ->select(
+            'a.companycode', 'a.plot', 'a.latitude', 'a.longitude',
+            'd.centerlatitude', 'd.centerlongitude',
+            'c.batchno', 'c.batchdate', 'c.batcharea', 'c.tanggalulangtahun',
+            'c.kodevarietas', 'c.kodestatus', 'c.jaraktanam', 'c.isactive',
+            'b.luasarea', 'b.jaraktanam as plot_jaraktanam', 'b.status'
+        )
+        ->get();
+    
+    // Get unique plot headers (center coordinates)
+    $plotHeaders = $plotData->map(function($item) {
+        return (object)[
+            'companycode' => $item->companycode,
+            'plot' => $item->plot,
+            'centerlatitude' => $item->centerlatitude,
+            'centerlongitude' => $item->centerlongitude
+        ];
+    })->unique('plot')->values();
+    
+    // Group activities by PC/RC (dari details)
+    $activities = $details->groupBy(function($item) {
+        // Asumsi: ada field yang menandakan PC/RC, misal 'activity_type' atau 'kategori'
+        // Sesuaikan dengan struktur data Anda
+        return $item->activity_type ?? 'PC'; // default PC jika tidak ada
+    });
+    
+    return view('dashboard.timeline-plot.index', [
         'title' => $title,
-        'nav'   => $nav
-      ]);
-    }
+        'nav' => $nav,
+        'navbar' => 'Timeline',
+        'rkhno' => $rkhno,
+        'plotHeaders' => $plotHeaders,
+        'plotData' => $plotData,
+        'activities' => $activities,
+        'details' => $details
+    ]);
+}
+
 
 }
