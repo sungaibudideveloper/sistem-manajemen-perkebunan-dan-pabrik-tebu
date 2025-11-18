5d29670c97812dc3b657cd9b9f83d3fed6104768
Renaldo Livando <renaldolivando6@gmail.com>
2025-11-18 14:41:08 +0700

    Tambah Report Masterlist (Batch Plot)

--- routes/report.php
+++ routes/report.php
@@ -2,10 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Input\HPTController;
+use App\Http\Controllers\Input\AgronomiController;
+
 use App\Http\Controllers\Report\PivotController;
 use App\Http\Controllers\Report\ReportController;
 use App\Http\Controllers\Report\PanenTebuController;
-use App\Http\Controllers\Input\AgronomiController;
+use App\Http\Controllers\Report\MasterLahanReportController;
+
+
 
 Route::group(['middleware' => ['auth', 'permission:Report Agronomi']], function () {
     Route::match(['GET', 'POST'], '/report/agronomi', [ReportController::class, 'agronomi'])->name('report.agronomi.index');
@@ -30,3 +34,9 @@ Route::get('report/agronomipivot', [PivotController::class, 'pivotTableAgronomi'
     ->middleware('permission:Pivot Agronomi');
 Route::get('report/hptpivot', [PivotController::class, 'pivotTableHPT'])->name('pivotTableHPT')
     ->middleware('permission:Pivot HPT');
+
+// Report Master Lahan
+Route::group(['middleware' => ['auth', 'permission:Report Manajemen Lahan']], function () {
+    Route::get('report/manajemen-lahan', [App\Http\Controllers\Report\MasterLahanReportController::class, 'index'])->name('report.report-manajemen-lahan.index');
+    Route::get('report/manajemen-lahan/data', [App\Http\Controllers\Report\MasterLahanReportController::class, 'getData'])->name('report.report-manajemen-lahan.data');
+});
