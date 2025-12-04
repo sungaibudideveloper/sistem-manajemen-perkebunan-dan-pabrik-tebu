e3446ce6a46c57ffbd785fa44438d7364383a408
Renaldo Livando <renaldolivando6@gmail.com>
2025-12-04 14:13:59 +0700

    Update hardcode mapping bsm route

--- app/View/Composers/NavigationComposer.php
+++ app/View/Composers/NavigationComposer.php
@@ -255,7 +255,6 @@ class NavigationComposer
             'gudang-bbm' => 'Menu Gudang',
             'kendaraan-workshop' => 'Kendaraan',
             'pias' => 'Menu Pias',
-            'mapping-bsm' => 'Mapping BSM',
 
             // Dashboard - non-standard permissions
             'agronomi-dashboard' => 'Dashboard Agronomi',
